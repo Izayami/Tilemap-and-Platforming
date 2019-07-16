31ae83c1e3775dfe366ac9154053c20dd0a79f37
Izayami <52263008+Izayami@users.noreply.github.com>
2019-07-15 23:40:44 -0400

    Finished
    Added the complete Script and also the functions for the 2D sprite

--- Tilemapping and Platforming/Assets/Scripts/PlayerController.cs
+++ Tilemapping and Platforming/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour{
 private Rigidbody2D rb2d;
 public float speed;
+public float jumpForce;
 
 void Start () {
     rb2d = GetComponent<Rigidbody2D>();
@@ -14,8 +15,19 @@ void Update(){
 }
 void FixedUpdate()
 {
+    if (Input.GetKey("escape"))
+     Application.Quit();
+
     float moveHorizontal = Input.GetAxis("Horizontal");
     Vector2 movement = new Vector2(moveHorizontal, 0);
     rb2d.AddForce(movement * speed);
+}
+void OnCollisionStay2D(Collision2D collision)
+{
+    if(collision.collider.tag == "Ground"){
+if(Input.GetKey(KeyCode.UpArrow)){
+    rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+}
+    }
 }
 }
