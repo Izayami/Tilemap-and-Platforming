eda21b7b96bf91771a7f02d1d731ae8adcbd54d7
Izayami <52263008+Izayami@users.noreply.github.com>
2019-07-18 21:37:46 -0400

    Added Components
    Added score and win text

--- Tilemapping and Platforming/Assets/Scripts/PlayerController.cs
+++ Tilemapping and Platforming/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour{
 private Rigidbody2D rb2d;
 public float speed;
 public float jumpForce;
 
+public Text countText;
+
+public Text winText;
+
+private int count;
+
+
 void Start () {
     rb2d = GetComponent<Rigidbody2D>();
+
+    count = 0;
+
+    winText.text = "";
+
 }
 void Update(){
 
@@ -22,11 +35,29 @@ void FixedUpdate()
     Vector2 movement = new Vector2(moveHorizontal, 0);
     rb2d.AddForce(movement * speed);
 }
+void OnTriggerEnter2D(Collider2D other)
+{
+    if (other.gameObject.CompareTag("Pickup"))
+                {
+                     other.gameObject.SetActive(false);
+                     count = count + 1;
+                     SetCountText();
+                }
+                void SetCountText()
+                {
+countText.text = "Count:" + count.ToString();
+if (count >= 4)
+winText.text = "You Win!";
+                }
+}
 void OnCollisionStay2D(Collision2D collision)
 {
     if(collision.collider.tag == "Ground"){
 if(Input.GetKey(KeyCode.UpArrow)){
     rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+}
+
+{
 }
     }
 }
